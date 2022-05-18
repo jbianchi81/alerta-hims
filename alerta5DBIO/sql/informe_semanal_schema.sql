a189e40c54306d9b397516d4b566d19d6075b06e
jbianchi81 <84208498+jbianchi81@users.noreply.github.com>
2022-05-18 16:00:28 -0300

    Add files via upload

--- alerta5DBIO/sql/informe_semanal_schema.sql
+++ alerta5DBIO/sql/informe_semanal_schema.sql
@@ -1,10 +1,14 @@
 BEGIN;
 
+-- DROP TABLE informe_semanal_contenido_region;
+-- DROP TABLE informe_semanal_regiones;
+-- DROP TABLE informe_semanal;
+
 CREATE TABLE informe_semanal_regiones (id varchar not null primary key,nombre varchar not null,geom geometry not null);
 
 CREATE TABLE informe_semanal (fecha date not null primary key, texto_general varchar);
 
-CREATE TABLE informe_semanal_contenido (fecha date references informe_semanal(fecha) NOT NULL ON DELETE CASCADE, region_id varchar references informe_semanal_regiones(id) NOT NULL ON DELETE CASCADE, texto varchar not null, unique (fecha, region_id));
+CREATE TABLE informe_semanal_contenido (fecha date references informe_semanal(fecha) ON DELETE CASCADE NOT NULL, region_id varchar references informe_semanal_regiones(id) ON DELETE CASCADE NOT NULL, texto varchar not null, unique (fecha, region_id));
 
 GRANT SELECT,INSERT,UPDATE,DELETE ON informe_semanal to actualiza;
 GRANT SELECT,INSERT,UPDATE,DELETE ON informe_semanal_regiones to actualiza;
